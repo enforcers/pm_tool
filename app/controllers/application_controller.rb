9492a0674c8c4c1624bf78ddfdd993e8c1d2d393
Lorenz Mintert <lorenz@mintert.org>
2013-12-21 12:07:41 +0100

    anfang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :get_all_projects
+
+  def get_all_projects
+  	@projects = Project.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 PmTool::Application.routes.draw do
+  get 'main/index'
+  resources :projects
+
+  root 'main#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
